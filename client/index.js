52efd41cdbb7d360cb0b7d3d1ebdf892083ce031
Glynn Phillips <glynndominicphillips@gmail.com>
2017-01-16 14:59:23 +0000

    Adds highlighting to speech body

--- client/index.js
+++ client/index.js
@@ -17,3 +17,34 @@
             - template.html
 
 */
+import axios from 'axios';
+
+function addHighlightMarkup(node, matcher, annotationIndex) {
+	node.innerHTML = node.innerHTML.replace(matcher, `<mark class="annotation-highlight" data-annotation="${annotationIndex}">${matcher}</mark>`)
+}
+
+function elementContainingAnnotationMatcher(matcher) {
+  let speechBody = document.querySelector('[data-speech-body]');
+	for (var i = 0; i < speechBody.childNodes.length; i++) {
+		if (speechBody.childNodes[i].textContent.includes(matcher)) {
+    	return speechBody.childNodes[i];
+			break;
+  	}
+	};
+}
+
+function highlightSpeech(annotations) {
+  annotations.forEach((annotation, index) => {
+		if(elementContainingAnnotationMatcher(annotation.match)) {
+    	addHighlightMarkup(elementContainingAnnotationMatcher(annotation.match), annotation.match, index);
+		}
+  });
+}
+
+axios.get(`http://bertha.ig.ft.com/view/publish/gss/17v6FLbDsDwxC7XGsqesLJiwq6CEd4FH3T8Erqawo9R4/authors,annotations`)
+  .then(data => {
+    console.log(data.data.annotations);
+		highlightSpeech(data.data.annotations);
+  }).catch(error => {
+    console.error(error);
+  });
