08b688bbce80280e9bc1c1ce40deacf6a0bf3ea6
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-01-16 10:57:00 +0000

    Adding data source

--- config/index.js
+++ config/index.js
@@ -1,32 +1,18 @@
 import article from './article';
 import getFlags from './flags';
 import getOnwardJourney from './onward-journey';
+import axios from 'axios';
 
 export default async function() {
   const d = await article();
   const flags = await getFlags();
   const onwardJourney = await getOnwardJourney();
-  /*
-  An experimental demo that gets content from the API
-  and overwrites some model values. This requires the Link File
-  to have been published. Also next-es-interface.ft.com probably
-  isn't a reliable source. Also this has no way to prevent development
-  values being seen in productions... use with care.
-
   try {
-    const a = (await axios(`https://next-es-interface.ft.com/content/${d.id}`)).data;
-    d.headline = a.title;
-    d.byline = a.byline;
-    d.summary = a.summaries[0];
-    d.title = d.title || a.title;
-    d.description = d.description || a.summaries[1] || a.summaries[0];
-    d.publishedDate = new Date(a.publishedDate);
-    f.comments = a.comments;
+    d.annotations = (await axios(`http://bertha.ig.ft.com/view/publish/gss/17v6FLbDsDwxC7XGsqesLJiwq6CEd4FH3T8Erqawo9R4/authors,annotations`)).data.annotations;
+    console.dir(d.annotations)
   } catch (e) {
-    console.log('Error getting content from content API');
+    console.error(e);
   }
-
-  */
 
   return {
     ...d,
