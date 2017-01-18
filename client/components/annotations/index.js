e3879573528c7605019420b70e0baa283bbb2ab0
Glynn Phillips <glynndominicphillips@gmail.com>
2017-01-18 15:46:20 +0000

    seperating styles from component

---
+++ client/components/annotations/index.js
@@ -0,0 +1,169 @@
+import axios from 'axios';
+import MarkdownIt from 'markdown-it';
+
+class Annotation {
+	constructor(rootElement, options) {
+		this.options = options;
+    this.rootElement = rootElement;
+		this.highlightAttribute = 'data-highlight';
+		this.highlightElements;
+		this.annotations;
+		this.selectedAnnotation;
+
+    this.getAnnotations();
+		this.appendAnnotation();
+	}
+
+  static init(rootElement, options = {}) {
+    options.minWidth = 200;
+    options.maxWidth = 400;
+    options.gutter = 40;
+
+    if (!rootElement) {
+      rootElement = document.querySelectorAll('[data-annotation-text]');
+    } else if (!(rootElement instanceof HTMLElement)) {
+      rootElement = document.querySelectorAll(rootElement);
+    }
+
+    if (rootElement instanceof HTMLElement) {
+      new Annotation(rootElement, options);
+    } else {
+      [].forEach.call(rootElement, (element) => {
+        options.annotationsId = element.getAttribute('data-annotation-id');
+        new Annotation(element, options)
+      });
+    }
+  }
+
+  bindListeners() {
+		const eventHandler = (event) => {
+				if (event.type === 'click' || (event.type === 'click' && event.keyCode === 13)) {
+          this.openAnnotation(event.target);
+          if (this.selectedHighlight) {
+            this.selectedHighlight.setAttribute('aria-expanded', 'false');
+          }
+          this.selectedHighlight = event.target;
+          this.selectedHighlight.setAttribute('aria-expanded', 'true');
+        }
+    };
+
+    [].forEach.call(this.highlightElements, (element) => {
+      element.addEventListener('click', eventHandler);
+      element.addEventListener('keyup', (event) => {
+        if (event.keyCode === 13) {
+				  this.openAnnotation(event.target);
+          if (this.selectedHighlight) {
+            this.selectedHighlight.setAttribute('aria-expanded', 'false');
+          }
+          this.selectedHighlight = event.target;
+          this.selectedHighlight.setAttribute('aria-expanded', 'true');
+        }
+      });
+		});
+	}
+
+	getAnnotations() {
+		axios.get(`http://bertha.ig.ft.com/view/publish/gss/${this.options.annotationsId}/authors,annotations`)
+  		.then(data => {
+				this.annotations = data.data.annotations;
+				this.addHighlighting();
+				this.bindListeners();
+  		}).catch(error => {
+    		console.error(error);
+  		});
+	}
+
+  addHighlighting() {
+  	this.annotations.forEach((annotation, index) => {
+			if(this.elementContainingAnnotationMatcher(annotation.match)) {
+				this.highlightMarkup(this.elementContainingAnnotationMatcher(annotation.match), annotation.match, index);
+			}
+  	});
+		this.highlightElements = this.rootElement.querySelectorAll(`[${this.highlightAttribute}]`);
+	}
+
+  highlightMarkup(node, matcher, annotationIndex) {
+		const highlight = document.createElement('mark');
+    highlight.innerHTML = matcher;
+		highlight.tabIndex = 1;
+    highlight.classList.add('speech--highlight');
+    highlight.setAttribute(this.highlightAttribute, annotationIndex);
+    highlight.setAttribute('aria-expanded', 'false');
+    highlight.setAttribute('aria-controls', 'annotation');
+
+    node.innerHTML = node.innerHTML.replace(matcher, highlight.outerHTML);
+	}
+
+  elementContainingAnnotationMatcher(matcher) {
+		for (var i = 0; i < this.rootElement.childNodes.length; i++) {
+			if (this.rootElement.childNodes[i].textContent.includes(matcher)) {
+    		return this.rootElement.childNodes[i];
+				break;
+  		}
+		};
+	}
+
+  appendAnnotation() {
+    const annotationWidth = this.calculateAnnotationWidth();
+		this.annotationModal = document.createElement('aside');
+		this.annotationModal.id = 'annotation';
+    this.annotationModal.setAttribute('aria-hidden', true);
+    this.annotationModal.setAttribute('aria-live', 'polite');
+		this.annotationModal.classList.add('speech--annotation');
+
+    if (annotationWidth) {
+      this.annotationModal.setAttribute('style', `width: ${annotationWidth}px; position: absolute; margin: 0`);
+    }
+
+    this.rootElement.appendChild(this.annotationModal);
+	}
+
+  openAnnotation(clickedElement) {
+    const annotationIndex = clickedElement.getAttribute(this.highlightAttribute);
+    this.annotationModal.innerHTML = this.generateAnnotationMarkup(this.annotations[annotationIndex]);
+
+    this.annotationModal.style.top = `${this.calculateAnnotationYPosition(clickedElement, this.annotationModal).top}px`;
+
+
+    this.annotationModal.style.left = `${this.calculateAnnotationYPosition(clickedElement, this.annotationModal).left}px`;
+
+    this.annotationModal.style.visibility = 'visible';
+
+    this.annotationModal.setAttribute('aria-hidden', false);
+
+    clickedElement.parentNode.insertBefore(this.annotationModal, clickedElement.nextSibling)
+  }
+
+  generateAnnotationMarkup(data) {
+    const md = new MarkdownIt();
+    return `<h3><span class="o-typography-subhead--standard">${data.author}</span></h3> ${md.render(data.annotation.md)}`;
+  }
+
+  calculateAnnotationWidth() {
+    const spaceForAnnotation = (document.documentElement.clientWidth - (this.options.gutter * 2)) - (this.rootElement.getBoundingClientRect().left + this.rootElement.clientWidth);
+
+
+    let width = spaceForAnnotation > this.options.minWidth ? spaceForAnnotation : null;
+
+    width = width > this.options.maxWidth ? this.options.maxWidth : width;
+
+    return width;
+  }
+  calculateAnnotationYPosition(highlight, annotation) {
+
+
+    const topOfHighlight = highlight.offsetTop;
+    const heightOfAnnotation = annotation.clientHeight;
+    const bottomOfSpeech = this.rootElement.clientHeight + this.rootElement.offsetTop;
+    const leftPosition = this.rootElement.clientWidth + this.options.gutter;
+
+
+
+    return {
+      top: topOfHighlight + heightOfAnnotation < bottomOfSpeech ? topOfHighlight : topOfHighlight - ((topOfHighlight + heightOfAnnotation) - bottomOfSpeech),
+      left: leftPosition
+    };
+  }
+}
+
+export default Annotation;
